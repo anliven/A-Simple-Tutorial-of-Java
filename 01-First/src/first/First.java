8f93557fe4e67a5fbf829ccd11eb95162d8dd2cf
Li Guowang <guowang.li@nokia.com>
2017-05-27 17:17:22 +0800

    add files 01 and 02

---
+++ 01-First/src/first/First.java
@@ -0,0 +1,12 @@
+package first;
+
+public class First {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+		System.out.println("################");
+		System.out.println("##    Java    ##");
+		System.out.println("################");
+	}
+
+}
