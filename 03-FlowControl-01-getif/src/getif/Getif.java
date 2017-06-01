85b5bb1e6ce22c0deeacbc10b58740ed19ecc101
Li Guowang <guowang.li@nokia.com>
2017-06-01 17:10:11 +0800

    add flow control cases

---
+++ 03-FlowControl-01-getif/src/getif/Getif.java
@@ -0,0 +1,13 @@
+package getif;
+
+public class Getif {
+	public static void main(String args[]) { // 主方法
+		int x = 45; // 声明int型变量x，并赋给初值
+		int y = 12; // 声明int型变量y，并赋给初值
+		if (x > y) { // 判断x是否大于y
+			System.out.println("变量x大于变量y"); // 如果条件成立，输出的信息
+		}
+		if (x < y) 
+			System.out.println("变量x小于变量y"); // 如果
+	}
+}
---
+++ 03-FlowControl-02-getifelse/src/getifelse/Getifelse.java
@@ -0,0 +1,18 @@
+package getifelse;
+
+public class Getifelse {
+	public static void main(String args[]) { // 主方法
+		int math = 100; // 声明int型局部变量，并赋给初值95
+		int english = 100; // 声明int型局部变量，并赋给初值56
+		if (math > 60) { // 使用if语句判断math是否大于60
+			System.out.println("数学及格"); // 条件成立时输出信息
+		} else {
+			System.out.println("数学没及格"); // 条件不成立输出的信息
+		}
+		if (english > 60) { // 判断英语成绩是否大于60
+			System.out.println("英语及格"); // 条件成立输出的信息
+		} else {
+			System.out.println("英语没及格"); // 条件不成立输出的信息
+		}
+	}
+}
