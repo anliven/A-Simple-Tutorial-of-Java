5ebea2ddff67b10064a523fc6607c6bcacb1132f
anliven <anliven@yeah.net>
2017-08-19 23:00:46 +0800

    update Chapter-04 cases

--- Chapter-04-FlowControl/src/pers/anliven/learningjava/chapter04/T04Switch.java
+++ Chapter-04-FlowControl/src/pers/anliven/learningjava/chapter04/T04Switch.java
@@ -14,6 +14,10 @@ public class T04Switch {
 		case 3:
 			System.out.println("Wednesday");
 			break;
+		case 4:
+		case 5:		
+			System.out.println("Working day");
+			break;			
 		default: // 可选语句
 			System.out.println("Sorry,I don't Know");
 		}
@@ -25,8 +29,10 @@ public class T04Switch {
  * 
  * ### switch语句 
  * switch多分支语句，实现“多选一”的选择场景 
- * switch语句中的表达式值和常量值，都必须为整型、字符型或字符串类型
- * default语句为可选的
+ * switch后面小括号中的表达式值和常量值，都必须为整型、字符型或字符串类型
+ * case后面的值可以是常量数值或常量表达式，但不能是变量或带有变量的表达式
+ * 功能相同的case语句可以合并，只需要在最后一个case语句中指定内容
+ * default语句为可选的，可以出现在任意位置，也可以省略
  * 如果表达式值不与任何case常量值相同，而且没有default语句，那么switch则不做任何处理
  * 
  */
--- Chapter-04-FlowControl/src/pers/anliven/learningjava/chapter04/T05While.java
+++ Chapter-04-FlowControl/src/pers/anliven/learningjava/chapter04/T05While.java
@@ -3,6 +3,8 @@ package pers.anliven.learningjava.chapter04;
 public class T05While {
 
 	public static void main(String args[]) {
+		
+		# 1到10的求和
 		int x = 1;
 		int sum = 0;
 		while (x <= 10) { // while循环语句，先判断条件是否成立，再执行循环体
@@ -10,6 +12,21 @@ public class T05While {
 			x++;
 		}
 		System.out.println("sum = " + sum);
+		
+		# 判断一个数（小于10位）的位数
+        int num = 987;
+        int count = 0;
+        if ((num>=0) && (num<=999999999)){
+            while(num != 0){
+                count++;
+                num/=10;
+            }
+            System.out.println("它是个"+count+"位的整数！");
+            
+        }else{
+            System.out.println("输入有误");
+        }
+		
 	}
 
 }
